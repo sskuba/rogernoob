6b7a582776044eddc617f019dfc72d80930f3f1e
sskuba <sskuba@hotmail.es>
2017-02-07 22:12:29 +0100

    first commit

---
+++ rogernoob/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rogernoob
+{
+    public partial class Form1 : Form
+    {
+        practicam6Entities p;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            p = new practicam6Entities();
+            productesBindingSource1.DataSource = p.productes;
+            
+        }
+    }
+}
